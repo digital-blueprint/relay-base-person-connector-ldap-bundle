25161d3378078cf6a046d5495008593abea95f1c
Patrizio Bekerle <patrizio@bekerle.com>
2021-11-18 13:33:26 +0100

    Add tests

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\LdapPersonProviderBundle\Tests;
 
+use ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle;
+use Dbp\Relay\BasePersonBundle\DbpRelayBasePersonBundle;
+use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
 use Dbp\Relay\LdapPersonProviderBundle\DbpRelayLdapPersonProviderBundle;
+use Nelmio\CorsBundle\NelmioCorsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
@@ -18,19 +24,26 @@ class Kernel extends BaseKernel
     public function registerBundles(): iterable
     {
         yield new FrameworkBundle();
+        yield new ApiPlatformBundle();
+        yield new DbpRelayBasePersonBundle();
+        yield new DbpRelayCoreBundle();
         yield new DbpRelayLdapPersonProviderBundle();
+        yield new NelmioCorsBundle();
+        yield new SecurityBundle();
+        yield new TwigBundle();
     }
 
     protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader)
     {
         $container->import('@DbpRelayCoreBundle/Resources/config/services_test.yaml');
+        $container->import('@DbpRelayLdapPersonProviderBundle/Resources/config/services_test.yaml');
         $container->extension('framework', [
             'test' => true,
             'secret' => '',
         ]);
 
         $container->extension('dbp_relay_ldap_person_provider', [
-            'secret_token' => 'secret-test',
+            'ldap' => [],
         ]);
     }
 }
---
+++ tests/PersonTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tugraz\Relay\TugrazBundle\Tests;
+
+use Adldap\Connections\ConnectionInterface;
+use Adldap\Models\User as AdldapUser;
+use Adldap\Query\Builder;
+use Adldap\Query\Grammar;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Dbp\Relay\LdapPersonProviderBundle\Service\LDAPApi;
+use Dbp\Relay\LdapPersonProviderBundle\Service\LDAPPersonProvider;
+use Dbp\Relay\LdapPersonProviderBundle\TestUtils\DummyLDAPApiProvider;
+use Mockery;
+
+class PersonTest extends ApiTestCase
+{
+    /**
+     * @var LDAPApi
+     */
+    private $api;
+
+    /**
+     * @var LDAPPersonProvider
+     */
+    private $provider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $ldapApiProvider = new DummyLDAPApiProvider();
+        $this->api = new LDAPApi(self::createClient()->getContainer(), $ldapApiProvider);
+        $this->api->setConfig([
+            'ldap' => [
+                'attributes' => [
+                    'email' => 'email',
+                    'birthday' => 'dateofbirth',
+                ]
+            ]
+        ]);
+
+        $this->provider = new LDAPPersonProvider($this->api);
+    }
+
+    public function testBasic()
+    {
+        $this->expectExceptionMessageMatches('/.*/');
+        $this->provider->getPerson('____nope____');
+    }
+
+    protected function newBuilder()
+    {
+        $connection = Mockery::mock(ConnectionInterface::class);
+
+        return new Builder($connection, new Grammar());
+    }
+
+    public function testLDAPParsing()
+    {
+        $user = new AdldapUser([
+            'cn' => ['foobar'],
+            'givenName' => ['John'],
+            'sn' => ['Doe'],
+        ], $this->newBuilder());
+
+        $person = $this->api->personFromUserItem($user, false);
+        $this->assertEquals('John', $person->getGivenName());
+        $this->assertEquals('Doe', $person->getFamilyName());
+    }
+
+    public function testBirthDateParsing()
+    {
+        $variants = ['1994-06-24', '1994-06-24 00:00:00'];
+        foreach ($variants as $variant) {
+            $user = new AdldapUser([
+                'cn' => ['foobar'],
+                'dateofbirth' => [$variant],
+            ], $this->newBuilder());
+
+            $person = $this->api->personFromUserItem($user, false);
+            $this->assertNotNull($person->getBirthDate());
+            $this->assertEquals('1994-06-24', $person->getBirthDate());
+        }
+    }
+}
