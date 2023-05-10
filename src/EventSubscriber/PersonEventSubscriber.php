1b3ab28b61866c236323478554de80454542ab13
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-05-10 15:08:43 +0200

    core bundle update and required adaptions

--- src/EventSubscriber/PersonEventSubscriber.php
+++ src/EventSubscriber/PersonEventSubscriber.php
@@ -7,8 +7,11 @@ namespace Dbp\Relay\BasePersonConnectorLdapBundle\EventSubscriber;
 use Dbp\Relay\BasePersonConnectorLdapBundle\Event\PersonPostEvent;
 use Dbp\Relay\BasePersonConnectorLdapBundle\Event\PersonPreEvent;
 use Dbp\Relay\BasePersonConnectorLdapBundle\Service\LDAPApi;
+use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Dbp\Relay\CoreBundle\LocalData\AbstractLocalDataEventSubscriber;
+use Dbp\Relay\CoreBundle\LocalData\LocalData;
 use Dbp\Relay\CoreBundle\LocalData\LocalDataPreEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 class PersonEventSubscriber extends AbstractLocalDataEventSubscriber
 {
@@ -20,12 +23,24 @@ class PersonEventSubscriber extends AbstractLocalDataEventSubscriber
             ];
     }
 
-    protected function onPreEvent(LocalDataPreEvent $preEvent, array $mappedQueryParameters)
+    protected function onPreEvent(LocalDataPreEvent $preEvent, array $localQueryAttributes)
     {
         $options = $preEvent->getOptions();
-        foreach ($mappedQueryParameters as $sourceParameter => $parameterValue) {
-            LDAPApi::addFilter($options, $sourceParameter, LDAPApi::CONTAINS_CI_FILTER_OPERATOR, $parameterValue);
+        foreach ($localQueryAttributes as $localQueryAttribute) {
+            LDAPApi::addFilter($options, $localQueryAttribute[self::LOCAL_QUERY_PARAMETER_SOURCE_ATTRIBUTE_KEY],
+                self::toLDAPFilterOperator($localQueryAttribute[self::LOCAL_QUERY_PARAMETER_OPERATOR_KEY]),
+                $localQueryAttribute[self::LOCAL_QUERY_PARAMETER_VALUE_KEY]);
         }
         $preEvent->setOptions($options);
+    }
+
+    private function toLDAPFilterOperator(string $localDataQueryOperator): string
+    {
+        switch ($localDataQueryOperator) {
+            case LocalData::LOCAL_QUERY_OPERATOR_CONTAINS_CI:
+                return LDAPApi::CONTAINS_CI_FILTER_OPERATOR;
+            default:
+                throw ApiError::withDetails(Response::HTTP_INTERNAL_SERVER_ERROR, sprintf('unknown local data query operator \'%s\'', $localDataQueryOperator));
+        }
     }
 }
