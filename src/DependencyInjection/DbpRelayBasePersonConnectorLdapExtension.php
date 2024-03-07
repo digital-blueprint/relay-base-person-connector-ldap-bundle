3a060cd1825a5c1ac71bcdd72da3d973bdc684ab
Christoph Reiter <reiter.christoph@gmail.com>
2024-03-07 16:37:05 +0100

    Add support for Symfony 6

--- src/DependencyInjection/DbpRelayBasePersonConnectorLdapExtension.php
+++ src/DependencyInjection/DbpRelayBasePersonConnectorLdapExtension.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 class DbpRelayBasePersonConnectorLdapExtension extends ConfigurableExtension
 {
-    public function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function loadInternal(array $mergedConfig, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
